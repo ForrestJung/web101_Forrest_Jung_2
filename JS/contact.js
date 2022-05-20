1cbe682ba35ef8c3799164dae83fe684d6580b69
ForrestJung <jungforrest@gmail.com>
2022-05-20 15:51:47 -0500

    Contact page more developed and js file started

---
+++ JS/contact.js
@@ -0,0 +1,32 @@
+
+
+/* const element = comments */
+
+
+let formsubmitted = () => {
+    let useremail = document.getElementById('email');
+/*         Element.id = 'useremail'; */
+    let usercomments = document.getElementById('comments');
+/*         Element.id = 'usercomments'; */
+    window.alert("Thank You! Your Form Has Been Submitted");
+    console.log("Form Submitted")
+    console.log('useremail')
+    console.log('usercomments')
+    console.log(useremail)
+    console.log(usercomments)
+}
+
+/* function thankYou(formsubmitted) {
+    window.alert("Thank You! Your Form Has Been Submitted");
+    console.log(formsubmitted);
+}
+ */
+/* const formsubmitted = () => {
+
+} */
+
+/* function thankYou() {
+    window.alert("Thank You! Your Form Has Been Submitted")
+    console.log('Contact Form Submitted');
+} */
+
